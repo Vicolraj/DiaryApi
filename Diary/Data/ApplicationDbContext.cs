ec36b00e4cd554da140bf0e0dae7a2a14c70ff0d
Vicolraj <vicolraj@gmail.com>
2025-08-30 15:19:23 +0100

    first commit

---
+++ Diary/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Diary.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Diary.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<DiaryEntry> DiaryEntries { get; set; }
+    }
+}
---
+++ Diary/Models/DiaryEntry.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Diary.Models
+{
+    public class DiaryEntry
+    {
+        [Required]
+        public string Title { get; set; } = string.Empty;
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Content { get; set; } = string.Empty;
+        public DateTime? CreatedDate { get; set; } = DateTime.Now;
+    }
+}
